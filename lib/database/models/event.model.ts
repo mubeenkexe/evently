0ead05ece40e362bbcdc76a640578f65c0005981
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-10 13:04:36 +0500

    feat: create and export interface

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,20 @@
-import { Schema, models, model } from "mongoose";
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IEvent extends Document {
+    _id: string;
+    title: string;
+    description?: string;
+    location?: string;
+    createdAt: Date;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    price?: string;
+    isFree: boolean;
+    url?: string;
+    category?: { _id: string; name: string };
+    organizer?: { _id: string; firstName: string; lastName: string };
+}
 
 const EventSchema = new Schema({
     title: { type: String, required: true },
